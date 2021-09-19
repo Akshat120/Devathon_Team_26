3648e5dcf9067c0ccbcf3011b3488ce06f560c5e
Zepurts <53970116+Akshat120@users.noreply.github.com>
2021-09-19 11:34:55 +0530

    Delete DB directory

--- DB/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const user = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    facultyEmail: String
-});
-
-module.exports = User = mongoose.model('user', user);
