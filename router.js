b6279fef7eeca6717d96f5e6c57be0c4964c0edf
Akshat <akshat.dhiman15@gmail.com>
2021-09-19 11:29:30 +0530

    functionality fixed

--- router.js
+++ router.js
@@ -1,18 +1,26 @@
-const express = require('express');  
+const express = require('express');
 const router = express.Router();
 const studentController = require('./controllers/studentController')
 
-router.get('/',studentController.home)
-router.get('/registerpage',studentController.registerpage)
-router.get('/login/:uname',studentController.dashboard)
-router.get('/login/documentupload/:uname',studentController.documentupload)
-router.get('/login/trackapplication/:uname',studentController.trackapplication)
+//home
+router.get('/', studentController.home)
 
-router.post('/login',studentController.login)
-router.post('/register',studentController.register)
-router.post('/createaccount',studentController.createaccount)
+//student
+router.get('/authpage', studentController.authenticate)
+router.get('/register', studentController.registerrr)
+router.get('/registerpage', studentController.registerpage)
+router.get('/login/:uname', studentController.dashboard)
+router.get('/login/documentupload/:uname', studentController.documentupload)
+router.get('/login/trackapplication/:uname', studentController.trackapplication)
+
+router.post('/login', studentController.login)
+router.post('/register', studentController.register)
+router.post('/createaccount', studentController.createaccount)
+
+
+//admin
+router.get('/authpage_admin', studentController.authenticate_admin)
+router.get('/register_admin', studentController.registerrr_admin)
 
 
 module.exports = router;
-
-
